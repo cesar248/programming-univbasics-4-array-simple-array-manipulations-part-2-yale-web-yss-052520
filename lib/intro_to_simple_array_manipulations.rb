8832354a582945208093616ee4a2467f04dca71e
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 01:42:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(array1, array2)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens", "cool"]
   more_favs = ["sports cars", "flatiron school"]
   my_favorite_things.concat(more_favs)
 end
